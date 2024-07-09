b077005a3b8f924c475823a6cc39b9d041ca5bdb
Silverlan <silverlann@gmail.com>
2024-07-09 21:42:58 +0200

    feat: add changes for core engine api changes

--- lua/entities/components/ambient_generic/shared.lua
+++ lua/entities/components/ambient_generic/shared.lua
@@ -1,4 +1,4 @@
-util.register_class("ents.AmbientGeneric",BaseEntityComponent)
+util.register_class("ents.AmbientGeneric", BaseEntityComponent)
 
 ents.AmbientGeneric.SF_PLAY_EVERYWHERE = 1
 ents.AmbientGeneric.SF_START_SILENT = 16
@@ -12,113 +12,140 @@ function ents.AmbientGeneric:Initialize()
 	BaseEntityComponent.Initialize(self)
 	self:AddEntityComponent(ents.COMPONENT_IO)
 	self:AddEntityComponent(ents.COMPONENT_SOUND)
-	
-	self:BindEvent(Entity.EVENT_HANDLE_KEY_VALUE,"HandleKeyValue")
-	self:BindEvent(ents.IOComponent.EVENT_HANDLE_INPUT,"HandleInput")
+
+	self:BindEvent(Entity.EVENT_HANDLE_KEY_VALUE, "HandleKeyValue")
+	self:BindEvent(ents.IOComponent.EVENT_HANDLE_INPUT, "HandleInput")
 end
 
-function ents.AmbientGeneric:HandleKeyValue(key,val)
-	if(key == "message") then
+function ents.AmbientGeneric:HandleKeyValue(key, val)
+	if key == "message" then
 		local sndComponent = self:GetEntity():GetComponent(ents.COMPONENT_SOUND)
-		if(sndComponent ~= nil) then sndComponent:SetSoundSource(val) end
-	elseif(key == "health") then
+		if sndComponent ~= nil then
+			sndComponent:SetSoundSource(val)
+		end
+	elseif key == "health" then
 		local sndComponent = self:GetEntity():GetComponent(ents.COMPONENT_SOUND)
-		if(sndComponent ~= nil) then sndComponent:SetGain(tonumber(val) /10.0) end
-	elseif(key == "preset") then
+		if sndComponent ~= nil then
+			sndComponent:SetGain(tonumber(val) / 10.0)
+		end
+	elseif key == "preset" then
 		self.m_preset = val -- TODO
-	elseif(key == "volstart") then
+	elseif key == "volstart" then
 		self.m_volStart = tonumber(val) -- TODO
-	elseif(key == "fadeinsecs") then
+	elseif key == "fadeinsecs" then
 		self.m_fadeInSecs = tonumber(val) -- TODO
-	elseif(key == "fadeoutsecs") then
+	elseif key == "fadeoutsecs" then
 		self.m_fadeOutSecs = tonumber(val) -- TODO
-	elseif(key == "pitch") then
+	elseif key == "pitch" then
 		local sndComponent = self:GetEntity():GetComponent(ents.COMPONENT_SOUND)
-		if(sndComponent ~= nil) then sndComponent:SetPitch(tonumber(val) /100.0) end
-	elseif(key == "pitchstart") then
+		if sndComponent ~= nil then
+			sndComponent:SetPitch(tonumber(val) / 100.0)
+		end
+	elseif key == "pitchstart" then
 		self.m_pitchStart = tonumber(val) -- TODO
-	elseif(key == "spinup") then
+	elseif key == "spinup" then
 		self.m_spinUp = tonumber(val) -- TODO
-	elseif(key == "spindown") then
+	elseif key == "spindown" then
 		self.m_spinDown = tonumber(val) -- TODO
-	elseif(key == "lfotype") then
+	elseif key == "lfotype" then
 		self.m_lfoType = tonumber(val) -- TODO
-	elseif(key == "lforate") then
+	elseif key == "lforate" then
 		self.n_lfoRate = tonumber(val) -- TODO
-	elseif(key == "lfomodpitch") then
+	elseif key == "lfomodpitch" then
 		self.m_lfoModPitch = tonumber(val) -- TODO
-	elseif(key == "lfomodvol") then
+	elseif key == "lfomodvol" then
 		self.m_lfoModVol = tonumber(val) -- TODO
-	elseif(key == "cspinup") then
+	elseif key == "cspinup" then
 		self.m_cSpinUp = tonumber(val) -- TODO
-	elseif(key == "radius") then
+	elseif key == "radius" then
 		local sndComponent = self:GetEntity():GetComponent(ents.COMPONENT_SOUND)
-		if(sndComponent ~= nil) then sndComponent:SetMaxDistance(tonumber(val)) end
-	elseif(key == "spawnflags") then
+		if sndComponent ~= nil then
+			sndComponent:SetMaxDistance(tonumber(val))
+		end
+	elseif key == "spawnflags" then
 		val = tonumber(val)
 		local sndComponent = self:GetEntity():GetComponent(ents.COMPONENT_SOUND)
-		if(sndComponent ~= nil) then
-			sndComponent:SetRelativeToListener(bit.band(val,ents.AmbientGeneric.SF_PLAY_EVERYWHERE) ~= 0)
-			sndComponent:SetPlayOnSpawn(bit.band(val,ents.AmbientGeneric.SF_START_SILENT) == 0)
-			sndComponent:SetLooping(bit.band(val,ents.AmbientGeneric.SF_IS_NOT_LOOPED) == 0)
+		if sndComponent ~= nil then
+			sndComponent:SetRelativeToListener(bit.band(val, ents.AmbientGeneric.SF_PLAY_EVERYWHERE) ~= 0)
+			sndComponent:SetPlayOnSpawn(bit.band(val, ents.AmbientGeneric.SF_START_SILENT) == 0)
+			sndComponent:SetLooping(bit.band(val, ents.AmbientGeneric.SF_IS_NOT_LOOPED) == 0)
 		end
-	elseif(key == "sourceentityname") then
+	elseif key == "sourceentityname" then
 		self.m_sourceEntityName = val
-	else return util.EVENT_REPLY_UNHANDLED end
+	else
+		return util.EVENT_REPLY_UNHANDLED
+	end
 	return util.EVENT_REPLY_HANDLED
 end
 
 function ents.AmbientGeneric:OnEntitySpawn()
-	if(self.m_sourceEntityName == nil) then return end
+	if self.m_sourceEntityName == nil then
+		return
+	end
 	local ent = self:GetEntity()
-	local attComponent = ent:AddComponent(ents.COMPONENT_ATTACHABLE)
-	if(attComponent ~= nil) then
-		local it = ents.iterator({ents.IteratorFilterEntity(self.m_sourceEntityName)})
+	local attComponent = ent:AddComponent(ents.COMPONENT_ATTACHMENT)
+	if attComponent ~= nil then
+		local it = ents.iterator({ ents.IteratorFilterEntity(self.m_sourceEntityName) })
 		local ent = it()
-		if(ent ~= nil) then attComponent:AttachToEntity(ent) end
+		if ent ~= nil then
+			attComponent:AttachToEntity(ent)
+		end
 	end
 end
 
 function ents.AmbientGeneric:PlaySound()
 	local sndComponent = self:GetEntity():GetComponent(ents.COMPONENT_SOUND)
-	if(sndComponent == nil) then return end
+	if sndComponent == nil then
+		return
+	end
 	sndComponent:Play()
 end
 
 function ents.AmbientGeneric:StopSound()
 	local sndComponent = self:GetEntity():GetComponent(ents.COMPONENT_SOUND)
-	if(sndComponent == nil) then return end
+	if sndComponent == nil then
+		return
+	end
 	sndComponent:Stop()
 end
 
 function ents.AmbientGeneric:ToggleSound()
 	local sndComponent = self:GetEntity():GetComponent(ents.COMPONENT_SOUND)
-	if(sndComponent == nil) then return end
-	if(sndComponent:IsPlaying()) then
+	if sndComponent == nil then
+		return
+	end
+	if sndComponent:IsPlaying() then
 		self:StopSound()
 		return
 	end
 	self:PlaySound()
 end
 
-function ents.AmbientGeneric:HandleInput(input,activator,caller,data)
-	if(input == "pitch") then
+function ents.AmbientGeneric:HandleInput(input, activator, caller, data)
+	if input == "pitch" then
 		local sndComponent = self:GetEntity():GetComponent(ents.COMPONENT_SOUND)
-		if(sndComponent ~= nil) then sndComponent:SetPitch(tonumber(data) /100.0) end
-	elseif(input == "playsound") then
+		if sndComponent ~= nil then
+			sndComponent:SetPitch(tonumber(data) / 100.0)
+		end
+	elseif input == "playsound" then
 		self:PlaySound()
-	elseif(input == "stopsound") then
+	elseif input == "stopsound" then
 		self:StopSound()
-	elseif(input == "togglesound") then
+	elseif input == "togglesound" then
 		self:ToggleSound()
-	elseif(input == "volume") then
+	elseif input == "volume" then
 		local sndComponent = self:GetEntity():GetComponent(ents.COMPONENT_SOUND)
-		if(sndComponent ~= nil) then sndComponent:SetGain(tonumber(data) /10.0) end
-	elseif(input == "fadein") then
+		if sndComponent ~= nil then
+			sndComponent:SetGain(tonumber(data) / 10.0)
+		end
+	elseif input == "fadein" then
 		-- TODO
-	elseif(input == "fadeout") then
+	elseif input == "fadeout" then
 		-- TODO
-	else return util.EVENT_REPLY_UNHANDLED end
+	else
+		return util.EVENT_REPLY_UNHANDLED
+	end
 	return util.EVENT_REPLY_HANDLED
 end
-ents.COMPONENT_AMBIENT_GENERIC = ents.register_component("ambient_generic",ents.AmbientGeneric,ents.EntityComponent.FREGISTER_NONE)
+ents.COMPONENT_AMBIENT_GENERIC =
+	ents.register_component("ambient_generic", ents.AmbientGeneric, ents.EntityComponent.FREGISTER_NONE)
--- lua/entities/components/func_trackautochange/shared.lua
+++ lua/entities/components/func_trackautochange/shared.lua
@@ -1,4 +1,4 @@
-util.register_class("ents.FuncTrackAutoChange",BaseEntityComponent)
+util.register_class("ents.FuncTrackAutoChange", BaseEntityComponent)
 
 ents.FuncTrackAutoChange.SF_AUTO_ACTIVATE_TRAIN = 1
 ents.FuncTrackAutoChange.SF_RELINK_TRACK = 2
@@ -16,28 +16,41 @@ ents.FuncTrackAutoChange.TRAIN_SAFE = 0
 ents.FuncTrackAutoChange.TRAIN_BLOCKING = 1
 ents.FuncTrackAutoChange.TRAIN_FOLLOWING = 2
 
-local defaultMemberFlags = bit.band(ents.BaseEntityComponent.MEMBER_FLAG_DEFAULT,bit.bnot(bit.bor(ents.BaseEntityComponent.MEMBER_FLAG_BIT_KEY_VALUE,ents.BaseEntityComponent.MEMBER_FLAG_BIT_INPUT,ents.BaseEntityComponent.MEMBER_FLAG_BIT_OUTPUT)))
-local defaultMemberFlagsNw = bit.bor(defaultMemberFlags,ents.BaseEntityComponent.MEMBER_FLAG_BIT_NETWORKED,ents.BaseEntityComponent.MEMBER_FLAG_TRANSMIT_ON_CHANGE)
+local defaultMemberFlags = bit.band(
+	ents.BaseEntityComponent.MEMBER_FLAG_DEFAULT,
+	bit.bnot(
+		bit.bor(
+			ents.BaseEntityComponent.MEMBER_FLAG_BIT_KEY_VALUE,
+			ents.BaseEntityComponent.MEMBER_FLAG_BIT_INPUT,
+			ents.BaseEntityComponent.MEMBER_FLAG_BIT_OUTPUT
+		)
+	)
+)
+local defaultMemberFlagsNw = bit.bor(
+	defaultMemberFlags,
+	ents.BaseEntityComponent.MEMBER_FLAG_BIT_NETWORKED,
+	ents.BaseEntityComponent.MEMBER_FLAG_TRANSMIT_ON_CHANGE
+)
 
-ents.FuncTrackAutoChange:RegisterMember("TravelAltitude",ents.MEMBER_TYPE_FLOAT,0.0,{},defaultMemberFlagsNw)
-ents.FuncTrackAutoChange:RegisterMember("SpinAmount",ents.MEMBER_TYPE_FLOAT,0.0,{},defaultMemberFlagsNw)
-ents.FuncTrackAutoChange:RegisterMember("TrainToSwitch",ents.MEMBER_TYPE_ENTITY,"",{},defaultMemberFlagsNw)
-ents.FuncTrackAutoChange:RegisterMember("TopTrack",ents.MEMBER_TYPE_ENTITY,"",{},defaultMemberFlagsNw)
-ents.FuncTrackAutoChange:RegisterMember("BottomTrack",ents.MEMBER_TYPE_ENTITY,"",{},defaultMemberFlagsNw)
+ents.FuncTrackAutoChange:RegisterMember("TravelAltitude", ents.MEMBER_TYPE_FLOAT, 0.0, {}, defaultMemberFlagsNw)
+ents.FuncTrackAutoChange:RegisterMember("SpinAmount", ents.MEMBER_TYPE_FLOAT, 0.0, {}, defaultMemberFlagsNw)
+ents.FuncTrackAutoChange:RegisterMember("TrainToSwitch", ents.MEMBER_TYPE_ENTITY, "", {}, defaultMemberFlagsNw)
+ents.FuncTrackAutoChange:RegisterMember("TopTrack", ents.MEMBER_TYPE_ENTITY, "", {}, defaultMemberFlagsNw)
+ents.FuncTrackAutoChange:RegisterMember("BottomTrack", ents.MEMBER_TYPE_ENTITY, "", {}, defaultMemberFlagsNw)
 
-ents.FuncTrackAutoChange:RegisterMember("SpawnFlags",ents.MEMBER_TYPE_UINT32,0,{},defaultMemberFlags)
+ents.FuncTrackAutoChange:RegisterMember("SpawnFlags", ents.MEMBER_TYPE_UINT32, 0, {}, defaultMemberFlags)
 
 function ents.FuncTrackAutoChange:__init()
 	BaseEntityComponent.__init(self)
 end
 function ents.FuncTrackAutoChange:Initialize()
 	BaseEntityComponent.Initialize(self)
-	self:AddEntityComponent(ents.COMPONENT_KINEMATIC_MOVER,"InitializeKinematicMover")
-	self:AddEntityComponent(ents.COMPONENT_RENDER,"InitializeRender")
-	
-	self:BindEvent(Entity.EVENT_HANDLE_KEY_VALUE,"HandleKeyValue")
-	self:BindEvent(ents.IOComponent.EVENT_HANDLE_INPUT,"HandleInput")
-	self:BindEvent(ents.KinematicMover.EVENT_ON_TARGET_REACHED,"OnTargetReached")
+	self:AddEntityComponent(ents.COMPONENT_KINEMATIC_MOVER, "InitializeKinematicMover")
+	self:AddEntityComponent(ents.COMPONENT_RENDER, "InitializeRender")
+
+	self:BindEvent(Entity.EVENT_HANDLE_KEY_VALUE, "HandleKeyValue")
+	self:BindEvent(ents.IOComponent.EVENT_HANDLE_INPUT, "HandleInput")
+	self:BindEvent(ents.KinematicMover.EVENT_ON_TARGET_REACHED, "OnTargetReached")
 end
 
 function ents.FuncTrackAutoChange:OnTargetReached()
@@ -52,23 +65,40 @@ function ents.FuncTrackAutoChange:OnTargetReached()
 	Also: Make sure to apply velocity to parented kinematic entities]]
 	local spinAmount = self:GetSpinAmount()
 	local trComponent = self:GetEntity():GetComponent(ents.COMPONENT_TRANSFORM)
-	if(trComponent ~= nil) then
-		trComponent:SetYaw(trComponent:GetYaw() +spinAmount)
+	if trComponent ~= nil then
+		trComponent:SetYaw(trComponent:GetYaw() + spinAmount)
 	end
-	
+
 	local entTrain = self:GetTrainToSwitch()
-	if(util.is_valid(entTrain)) then entTrain:RemoveComponent(ents.COMPONENT_ATTACHABLE) end
+	if util.is_valid(entTrain) then
+		entTrain:RemoveComponent(ents.COMPONENT_ATTACHMENT)
+	end
 end
 
 function ents.FuncTrackAutoChange:OnEntitySpawn()
-	if(self.m_trainName ~= nil) then
-		self:SetTrainToSwitch(ents.iterator(bit.bor(ents.ITERATOR_FILTER_DEFAULT,ents.ITERATOR_FILTER_BIT_PENDING),{ents.IteratorFilterEntity(self.m_trainName)})())
+	if self.m_trainName ~= nil then
+		self:SetTrainToSwitch(
+			ents.iterator(
+				bit.bor(ents.ITERATOR_FILTER_DEFAULT, ents.ITERATOR_FILTER_BIT_PENDING),
+				{ ents.IteratorFilterEntity(self.m_trainName) }
+			)()
+		)
 	end
-	if(self.m_topTrackName ~= nil) then
-		self:SetTopTrack(ents.iterator(bit.bor(ents.ITERATOR_FILTER_DEFAULT,ents.ITERATOR_FILTER_BIT_PENDING),{ents.IteratorFilterEntity(self.m_topTrackName)})())
+	if self.m_topTrackName ~= nil then
+		self:SetTopTrack(
+			ents.iterator(
+				bit.bor(ents.ITERATOR_FILTER_DEFAULT, ents.ITERATOR_FILTER_BIT_PENDING),
+				{ ents.IteratorFilterEntity(self.m_topTrackName) }
+			)()
+		)
 	end
-	if(self.m_bottomTrackName ~= nil) then
-		self:SetBottomTrack(ents.iterator(bit.bor(ents.ITERATOR_FILTER_DEFAULT,ents.ITERATOR_FILTER_BIT_PENDING),{ents.IteratorFilterEntity(self.m_bottomTrackName)})())
+	if self.m_bottomTrackName ~= nil then
+		self:SetBottomTrack(
+			ents.iterator(
+				bit.bor(ents.ITERATOR_FILTER_DEFAULT, ents.ITERATOR_FILTER_BIT_PENDING),
+				{ ents.IteratorFilterEntity(self.m_bottomTrackName) }
+			)()
+		)
 	end
 end
 
@@ -81,17 +111,27 @@ function ents.FuncTrackAutoChange:InitializeRender(component)
 	component:SetCastShadows(true)
 end
 
-function ents.FuncTrackAutoChange:HandleKeyValue(key,val)
-	if(key == "height") then self:SetTravelAltitude(tonumber(val))
-	elseif(key == "spawnflags") then self:SetSpawnFlags(tonumber(val))
-	elseif(key == "rotation") then self:SetSpinAmount(tonumber(val))
-	elseif(key == "train") then self.m_trainName = val
-	elseif(key == "toptrack") then self.m_topTrackName = val
-	elseif(key == "bottomtrack") then self.m_bottomTrackName = val
-	elseif(key == "speed") then
+function ents.FuncTrackAutoChange:HandleKeyValue(key, val)
+	if key == "height" then
+		self:SetTravelAltitude(tonumber(val))
+	elseif key == "spawnflags" then
+		self:SetSpawnFlags(tonumber(val))
+	elseif key == "rotation" then
+		self:SetSpinAmount(tonumber(val))
+	elseif key == "train" then
+		self.m_trainName = val
+	elseif key == "toptrack" then
+		self.m_topTrackName = val
+	elseif key == "bottomtrack" then
+		self.m_bottomTrackName = val
+	elseif key == "speed" then
 		local componentKinematicMover = self:GetEntity():GetComponent(ents.COMPONENT_KINEMATIC_MOVER)
-		if(componentKinematicMover ~= nil) then componentKinematicMover:SetSpeed(tonumber(val)) end
-	else return util.EVENT_REPLY_UNHANDLED end
+		if componentKinematicMover ~= nil then
+			componentKinematicMover:SetSpeed(tonumber(val))
+		end
+	else
+		return util.EVENT_REPLY_UNHANDLED
+	end
 	return util.EVENT_REPLY_HANDLED
 end
 
@@ -100,21 +140,34 @@ function ents.FuncTrackAutoChange:Trigger()
 	local entTopTrack = self:GetTopTrack()
 	local ent = self:GetEntity()
 	local componentKinematicMover = ent:GetComponent(ents.COMPONENT_KINEMATIC_MOVER)
-	if(componentKinematicMover == nil or util.is_valid(entBottomTrack) == false or util.is_valid(entTopTrack) == false) then return end
+	if
+		componentKinematicMover == nil
+		or util.is_valid(entBottomTrack) == false
+		or util.is_valid(entTopTrack) == false
+	then
+		return
+	end
 	local entTrain = self:GetTrainToSwitch()
-	if(util.is_valid(entTrain)) then
-		local attachableComponent = entTrain:AddComponent(ents.COMPONENT_ATTACHABLE)
-		if(attachableComponent ~= nil) then
+	if util.is_valid(entTrain) then
+		local attachableComponent = entTrain:AddComponent(ents.COMPONENT_ATTACHMENT)
+		if attachableComponent ~= nil then
 			attachableComponent:AttachToEntity(ent)
 		end
 	end
-	componentKinematicMover:SetMoveTarget(entBottomTrack:GetPos() -entTopTrack:GetPos())
+	componentKinematicMover:SetMoveTarget(entBottomTrack:GetPos() - entTopTrack:GetPos())
 	componentKinematicMover:StartForward()
 end
 
-function ents.FuncTrackAutoChange:HandleInput(input,activator,caller,data)
-	if(input == "trigger") then self:Trigger()
-	else return util.EVENT_REPLY_UNHANDLED end
+function ents.FuncTrackAutoChange:HandleInput(input, activator, caller, data)
+	if input == "trigger" then
+		self:Trigger()
+	else
+		return util.EVENT_REPLY_UNHANDLED
+	end
 	return util.EVENT_REPLY_HANDLED
 end
-ents.COMPONENT_FUNC_TRACKAUTOCHANGE = ents.register_component("func_trackautochange",ents.FuncTrackAutoChange,ents.EntityComponent.FREGISTER_BIT_NETWORKED)
+ents.COMPONENT_FUNC_TRACKAUTOCHANGE = ents.register_component(
+	"func_trackautochange",
+	ents.FuncTrackAutoChange,
+	ents.EntityComponent.FREGISTER_BIT_NETWORKED
+)
